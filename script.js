f348bc7914aa050e853915d37cbeb5dc67613ccc
matsubo-kuri <zenraiti@gmail.com>
2026-08-21 19:10:39 +0900

    Add button click event to change title text

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// ボタンとタイトルの要素を取得
+const button = document.getElementById('change-btn');
+const title = document.getElementById('title');
 
+// ボタンがクリックされたときの処理
+button.addEventListener('click', () => {
+    // タイトルの文字を変更
+    title.textContent = "ようこそ！";
+    title.style.color = "#ff4757";
+    
+    // アラートを表示
+    alert("文字が変わりました！");
+});
